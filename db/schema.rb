8702cc91b46ca7899dd812b652df5d656de668ef
Hiroharu Hara <hh.25799623@gmail.com>
2020-05-06 21:25:02 +0900

    [add] Userモデルを追加（カラムは現時点ではnameとemailのみ）

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_05_06_121834) do
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
